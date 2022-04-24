f7f74898510ecca661d08b058a4098a2f2fa52e7
oscarpergler <70218472+oscarpergler@users.noreply.github.com>
2022-04-24 15:42:51 +0200

    Added a way to send data to my second activity & added a signout button.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -7,15 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signIn;
+    private EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.signin_name);
 
         signIn = findViewById(R.id.signin_button);
         signIn.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Log.d("==>", "Signed in!");
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("username", username.getText(). toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,13 +2,39 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button signout;
+    private TextView usernameview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        signout = findViewById(R.id.signout_button);
+        usernameview = findViewById(R.id.welcome_text);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        String username = extras.getString("username");
+
+        usernameview.setText("Välkommen: " + username);
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
